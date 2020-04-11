fae1e0fb46a20c448b328b608cd757d335c6c9ef
reza <rezasemyari1991@gmail.com>
2020-04-11 19:51:33 +0430

    delete setTimeout.js

--- setTimeout.js
+++
@@ -1,7 +0,0 @@
-function func(){
-    return (console.log(new Date().toLocaleDateString()) );
-     
-}
-
-let timerId = setInterval(func, 1000)
-setTimeout(() => { clearInterval(timerId); alert('stop'); }, 10000);
